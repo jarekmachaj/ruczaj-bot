f7c1457f01f5eadf0a2e8c478cf456df88bb8ee6
Jarek Machaj <czemuja@gmail.com>
2016-10-02 16:20:44 +0200

    next

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.listen(process.env.PORT || 3000);
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
 
 // Server frontpage
 app.get('/', function (req, res) {
