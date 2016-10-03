4b679c5f1a35d5c1f2d88de22eb987a22a8dd86e
Jarek Machaj <czemuja@gmail.com>
2016-10-03 16:55:46 +0200

    logging user

--- index.js
+++ index.js
@@ -50,9 +50,10 @@ app.post('/webhook', function (req, res) {
 
 var token =  process.env.RUCZAJ_ACCESS_TOKEN;
 
-function getUserDetails(senderid) {
+function getUserDetails(senderid) {    
     var res = synRequest('GET', 'https://graph.facebook.com/v2.6/' + senderid + '?fields=first_name&access_token=' + token);
     var user = JSON.parse(res.getBody('utf8'));
+    console.log(user);
     return user;
 }
 
