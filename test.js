ac85ef0252887b194ebd19b6858fdf60ae630b53
Jarek Machaj <czemuja@gmail.com>
2016-10-03 17:44:46 +0200

    Clean up mess

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var synRequest = require('sync-request');
 
-var senderid = 100000280790482;
-var token = 'EAAItVgJ8SpABACg2ZANkfX3xvMS6mY3ZBxvfyfGOHrZBdxSAsRVC44Wy0RdXB1SvRPxEvfMYZBXz4RuB9iuMnIpyg1xmm4EiVK5bouGTVoZAyIMToZCrVB1gyJ2ujQxg1QsZBPhsjAVs3KXBPjOXCVX99ZAIOYAnS8qdfumglynoEgZDZD';
+var senderid = 278725065568764;
+var token = '';
 var res = synRequest('GET', 'https://graph.facebook.com/v2.6/' + senderid + '?fields=first_name&access_token=' + token);
 var user = JSON.parse(res.getBody('utf8'));
 console.log(user.first_name);
