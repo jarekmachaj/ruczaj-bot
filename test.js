769889cc29d1919b81df14a82ce143c7f09db901
Jarek Machaj <jarekmachaj@users.noreply.github.com>
2016-10-03 17:47:30 +0200

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-var synRequest = require('sync-request');
-
-var senderid = 278725065568764;
-var token = '';
-var res = synRequest('GET', 'https://graph.facebook.com/v2.6/' + senderid + '?fields=first_name&access_token=' + token);
-var user = JSON.parse(res.getBody('utf8'));
-console.log(user.first_name);
