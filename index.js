b5ea604f661da974bb16414520a826011efeea62
Jarek Machaj <czemuja@gmail.com>
2016-10-04 17:28:28 +0200

    ret

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 
-
-
 // Server frontpage
 app.get('/', function (req, res) {
     res.send('This is Ruczaj-bot Server - beta');
